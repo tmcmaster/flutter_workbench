10c1c26bf9b9ab988b8a94e250b5d2515ed077da
Tim McMaster <tim@mcmaster.id.au>
2021-07-10 10:31:43 +1000

    added the widget_examples_tester library to the flutter_workbench

--- lib/flutter_workbench.dart
+++ lib/flutter_workbench.dart
@@ -5,6 +5,7 @@ export 'package:prototype_widget/prototype_widget.dart';
 export 'package:scaffold_widget/scaffold_widget.dart';
 export 'package:screen_tester/screen_tester.dart';
 export 'package:theme_builder/theme_builder.dart';
+export 'package:widget_examples_tester/widget_examples_tester.dart';
 export 'package:widget_tester/widget_tester.dart';
 
 export 'src/flutter_workbench.dart';
